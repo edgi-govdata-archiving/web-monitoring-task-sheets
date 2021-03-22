29fb3757a2e2545d709e87cae7cae37ecdd65653
Rob Brackett <rob@robbrackett.com>
2021-03-21 18:26:33 -0700

    Update CLI interface
    This is a pretty big and ugly update to `generate_task_sheets.py`, but not a real change in practice. We've been running this code in production for a while, and I kept meaning to clean it up and commit it. Instead it got messier as it accumulated changes and hacks. I never should have waited to commit -- it's time to get this into version control, and *then* clean it up.

--- generate_task_sheets.py
+++ generate_task_sheets.py
@@ -10,71 +10,77 @@ from retry import retry
 import signal
 import sys
 from tqdm import tqdm
+import threading
 import traceback
 from web_monitoring import db
 
 
-def list_all_pages(url_pattern, after, before, cancel=None, client=None, total=False):
-    if client is None:
-        client = db.Client.from_env()
+client_storage = threading.local()
+def get_thread_client():
+    if not hasattr(client_storage, 'client'):
+        client_storage.client = db.Client.from_env()
+    return client_storage.client
 
-    chunk = 1
-    while chunk > 0:
+
+def list_all_pages(url_pattern, after, before, tags=None, cancel=None, client=None, total=False):
+    client = client or get_thread_client()
+
+    pages = client.get_pages(url=url_pattern,
+                             tags=tags,
+                             active=True,
+                             include_total=total,
+                             # start_date=after, end_date=before,
+                             # include_earliest=True,
+                             sort=['created_at:asc'],
+                             chunk_size=500)
+
+    # Handle canceling and emit total as first item
+    yielded_total = total is False
+    for page in pages:
+        if not yielded_total:
+            yield page['_list_meta']['total_results']
+            yielded_total = True
+        yield page
         if cancel and cancel.is_set():
             return
 
-        pages = request_page_chunk(client, sort=['created_at:asc'],
-                                   chunk_size=1000, chunk=chunk,
-                                   url=url_pattern, active=True,
-                                   start_date=after, end_date=before,
-                                   include_earliest=True,
-                                   include_total=(total and chunk == 1))
-        if total and chunk == 1:
-            yield pages["meta"]["total_results"]
-        yield from pages['data']
-        chunk = pages['links']['next'] and (chunk + 1) or -1
 
-
-def list_page_versions(page_id, after, before, cancel=None, client=None):
-    if client is None:
-        client = db.Client.from_env()
-
-    common = dict(page_id=page_id,
-                  # include_change_from_previous=True,
-                  # include_change_from_earliest=True,
-                  sort=['capture_time:desc'])
-
-    chunk = 1
-    while chunk > 0:
-        if cancel and cancel.is_set():
-            return
-
-        result = request_version_chunk(client, chunk=chunk, chunk_size=1000,
-                                       start_date=after, end_date=before,
-                                       **common)
-        yield from result['data']
-        chunk = result['links']['next'] and (chunk + 1) or -1
+def list_page_versions(page_id, after, before, chunk_size=1000, cancel=None,
+                       client=None):
+    client = client or get_thread_client()
 
     if cancel and cancel.is_set():
         return
 
+    versions = client.get_versions(page_id=page_id,
+                                   start_date=after, end_date=before,
+                                   # include_change_from_previous=True,
+                                   # include_change_from_earliest=True,
+                                   sort=['capture_time:desc'],
+                                   chunk_size=chunk_size)
+
+    for version in versions:
+        yield version
+        if cancel and cancel.is_set():
+            return
+
     # Get version leading into timeframe.
-    result = request_version_chunk(client, chunk_size=1,
-                                   start_date=None, end_date=after,
-                                   **common)
-    yield from result['data']
+    if after:
+        yield next(client.get_versions(page_id=page_id,
+                                       start_date=None, end_date=after,
+                                       sort=['capture_time:desc'],
+                                       chunk_size=1))
 
 
-@retry(tries=3, delay=1)
-def request_page_chunk(client, **kwargs):
-    with ActivityMonitor(f'List pages: {kwargs}'):
-        return client.list_pages(**kwargs)
+def get_earliest_version(page_id, cancel=None, client=None):
+    client = client or get_thread_client()
 
+    if cancel and cancel.is_set():
+        return
 
-@retry(tries=3, delay=1)
-def request_version_chunk(client, **kwargs):
-    with ActivityMonitor(f'List versions: {kwargs}'):
-        return client.list_versions(**kwargs)
+    return next(client.get_versions(page_id=page_id,
+                                    sort=['capture_time:asc'],
+                                    chunk_size=1))
 
 
 def add_versions_to_page(page, after, before):
@@ -82,13 +88,95 @@ def add_versions_to_page(page, after, before):
     Find all the relevant versions of a page in the given timeframe and attach
     them to the page object as 'versions'.
     """
-    versions = list(list_page_versions(page['uuid'], after, before))
+    def in_time_range(version):
+        return version['capture_time'] >= after and version['capture_time'] < before
+
+    def get_status(version):
+        status = version.get('status') or 200
+        if status == 200 and version.get('content_length', -1) == 0:
+            return 500
+        else:
+            return status
+
+    all_versions = list_page_versions(page['uuid'], None, before, chunk_size=20)
+    versions = []
+    # Start with a dummy version to handle the case where there is no version
+    # in the timeframe.
+    version_after = {'status': -1}  # Dummy version to ensure non-matches
+    for version in all_versions:
+        in_timeframe = in_time_range(version)
+        if in_timeframe:
+            versions.append(version)
+        elif get_status(version) < 400 or not version_after:
+            # Check `version_after` because there may have been no versions in
+            # the timeframe.
+            versions.append(version)
+            break
+        else:
+            # If the version preceeding the timeframe was an error, check
+            # whether it was intermittent and use the one before it if so.
+            final_version = version
+            try:
+                version_before = next(all_versions)
+                # Compare the status codes rather than check whether they are
+                # "OK" because the baseline state may also have been an error.
+                version_status = get_status(version)
+                before_status = get_status(version_before)
+                after_status = get_status(version_after)
+                if before_status == after_status and version_status != before_status:
+                    final_version = version_before
+            except StopIteration:
+                pass
+
+            versions.append(final_version)
+            break
+
+        version_after = version
+
+    if len(versions) >= 2:
+        page['earliest'] = get_earliest_version(page['uuid'])
+    else:
+        # Since there aren't at least two versions to compare, this page won't
+        # actually get analyzed, so don't bother loading the earliest version.
+        # Set an empty value so we can safely check the `earliest` key.
+        page['earliest'] = None
+
+    # all_versions = list(list_page_versions(page['uuid'], None, before))
+    # page['earliest'] = all_versions[-1] if len(all_versions) > 0 else None
+    # # versions = list(filter(in_time_range, all_versions))
+    # versions = []
+    # for index, version in enumerate(all_versions):
+    #     in_timeframe = in_time_range(version)
+    #     if in_timeframe:
+    #         versions.append(version)
+    #     elif get_status(version) < 400:
+    #         versions.append(version)
+    #         break
+    #     else:
+    #         # If the version preceeding the timeframe was an error, check
+    #         # whether it was intermittent and use the one before it if so.
+    #         final_version = version
+    #         if len(all_versions) > index + 1:
+    #             # Compare the status codes rather than check whether they are
+    #             # "OK" because the baseline state may also have been an error.
+    #             version_status = get_status(version)
+    #             before_status = get_status(all_versions[index + 1])
+    #             after_status = get_status(all_versions[index - 1])
+    #             if before_status == after_status and version_status != before_status:
+    #                 final_version = all_versions[index + 1]
+
+    #         versions.append(final_version)
+    #         break
+
+
+    # page['earliest'] = get_earliest_version(page['uuid'])
+    # versions = list(list_page_versions(page['uuid'], after, before))
 
     # If the latest version is an error but the page is not in an error state,
     # then the error was spurious and should not be part of the analysis.
-    if page['status'] is None or page['status'] < 400:
+    if get_status(page) < 400:
         error_versions = []
-        while versions and versions[0].get('status', 200) >= 400:
+        while versions and get_status(versions[0]) >= 400:
             error_versions.append(versions.pop(0))
 
         if error_versions:
@@ -148,6 +236,9 @@ def log_error(output, verbose, item):
 
 def pretty_print_analysis(page, analysis, output=None):
     message = [f'{page["url"]} ({page["uuid"]}):']
+    a = page['versions'][len(page['versions']) - 1]
+    b = page['versions'][0]
+    message.append(f'  a: {a["uuid"]}\n  b: {b["uuid"]}')
     for key, value in analysis.items():
         message.append(f'  {key}: {value}')
 
@@ -158,30 +249,71 @@ def pretty_print_analysis(page, analysis, output=None):
         print(message, file=sys.stderr)
 
 
-def main(pattern=None, after=None, before=None, output_path=None, threshold=0, verbose=False):
+def main(pattern=None, tags=None, after=None, before=None, output_path=None, threshold=0, verbose=False):
     with QuitSignal((signal.SIGINT,)) as cancel:
         # Make sure we can actually output the results before getting started.
         if output_path:
             output_path.mkdir(exist_ok=True)
 
         pages = generate_on_thread(list_all_pages, pattern, after, before,
-                                   cancel=cancel, total=True).output
+                                   tags, cancel=cancel, total=True).output
 
         page_count = next(pages)
         pages_and_versions = map_parallel(add_versions_to_page, pages, after,
                                           before, cancel=cancel,
-                                          parallel=5).output
+                                          parallel=4).output
 
         # Separate progress bar just for data loading from Scanner's DB
-        # pages_and_versions = tqdm(pages_and_versions, desc='  loading',
-        #                           unit=' pages', total=page_count)
+        pages_and_versions = tqdm(pages_and_versions, desc='  loading',
+                                  unit=' pages', total=page_count)
 
-        results = analyze.analyze_pages(pages_and_versions, after, before, cancel=cancel)
+        def iterate_all(source):
+            yield from source
+        pages_and_versions_queue = generate_on_thread(iterate_all, pages_and_versions, cancel=cancel).output
+        results = analyze.analyze_pages(pages_and_versions_queue, after, before, cancel=cancel)
+        # results = analyze.analyze_pages(pages_and_versions, after, before, cancel=cancel)
         progress = tqdm(results, desc='analyzing', unit=' pages', total=page_count)
         # Log any unexpected errors along the way.
         results = tap(progress, lambda result: log_error(tqdm, verbose, result))
         # Don't output pages where there was no overall change.
         results = (item for item in results if not isinstance(item[2], analyze.NoChangeError))
+
+        # DEBUG READABILITY FALLBACK CODE
+        results = list(results)
+        no_content_readable = []
+        no_content_unreadable = []
+        for page, analysis, error in results:
+            if not error and not analysis['text']['found_content_area']:
+                if analysis['redirect']['is_redirect']:
+                    continue
+                a_id = page['versions'][len(page['versions']) - 1]['uuid']
+                b_id = page['versions'][0]['uuid']
+                if analysis['text']['readable']:
+                    no_content_readable.append((page['url'], page['uuid'], a_id, b_id))
+                else:
+                    no_content_unreadable.append((page['url'], page['uuid'], a_id, b_id))
+        def sortable_entry(entry):
+            if entry[0].startswith('http:'):
+                return (f'https{entry[0][4:]}', *entry[1:])
+            return entry
+        no_content_readable.sort(key=sortable_entry)
+        no_content_unreadable.sort(key=sortable_entry)
+        readability_debug = []
+        if len(no_content_readable):
+            readability_debug.append('No content found, but readability worked:')
+            for url, page_id, a_id, b_id in no_content_readable:
+                readability_debug.append(f'  {url}: https://api.monitoring.envirodatagov.org/api/v0/pages/{page_id}/changes/{a_id}..{b_id}')
+        if len(no_content_unreadable):
+            readability_debug.append('No content found, AND readability failed:')
+            for url, page_id, a_id, b_id in no_content_unreadable:
+                readability_debug.append(f'  {url}: https://api.monitoring.envirodatagov.org/api/v0/pages/{page_id}/changes/{a_id}..{b_id}')
+        if output_path:
+            with (output_path / '_readability_debug.txt').open('w') as f:
+                f.write('\n'.join(readability_debug) + '\n')
+        else:
+            print('\n'.join(readability_debug), file=sys.stderr)
+        # END DEBUG READABLILITY FALLBACK
+
         # Filter out results under the threshold
         results = filter(lambda item: item[1] is None or item[1]['priority'] > threshold,
                          results)
@@ -189,7 +321,7 @@ def main(pattern=None, after=None, before=None, output_path=None, threshold=0, v
         # If we aren't writing to disk, just print the high-priority results.
         if not output_path:
             for page, analysis, error in results:
-                if analysis and analysis['priority'] >= 0.5:
+                if analysis: # and analysis['priority'] >= 0: # 0.5:
                     pretty_print_analysis(page, analysis, tqdm)
             return
 
@@ -206,6 +338,10 @@ def main(pattern=None, after=None, before=None, output_path=None, threshold=0, v
                            for result in sorted_groups
                            for item in result['items'])
             write_csv(output_path, sheet_name, sorted_rows)
+
+        # Clear the last line from TQDM, which seems to leave behind the second
+        # progress bar. :\
+        print('', file=sys.stderr)
 
 
 def timeframe_date(date_string):
@@ -227,6 +363,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Count term changes in monitored pages.')
     parser.add_argument('--output', type=Path, help='Output CSV files in this directory')
     parser.add_argument('--pattern', help='Only analyze pages with URLs matching this pattern.')
+    parser.add_argument('--tag', action='append', help='Only anlyze pages with this tag (repeat for multiple tags).')
     parser.add_argument('--after', type=timeframe_date, help='Only include versions after this date. May also be a number of hours before the current time.')
     parser.add_argument('--before', type=timeframe_date, help='Only include versions before this date. May also be a number of hours before the current time.')
     parser.add_argument('--threshold', type=float, default=0.15, help='Minimum priority value to include in output.')
@@ -248,6 +385,7 @@ if __name__ == '__main__':
         options.before = datetime.now(tz=timezone.utc)
 
     main(pattern=options.pattern,
+         tags=(options.tag or None),
          before=options.before,
          after=options.after,
          output_path=options.output,
